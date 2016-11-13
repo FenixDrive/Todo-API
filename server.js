4535c45250f62aeddb38499e3ba9838d845b92f0
FenixDrive <davidbsmart@hotmail.com>
2016-11-13 17:41:18 +1300

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1, 
-	description: 'Meet dad for dinner',
-	completed: false
-}, {
-	id: 2, 
-	description: 'Go to the supermarket',
-	completed: false
-}, {
-	id: 3, 
-	description: 'Download Postman',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -46,6 +40,18 @@ app.get('/todos/:id', function (req, res) {
 });
 //Original mistakes were not having a / in front of todos and using paramas instead of paramas
 
+
+//new http method to add data to todos
+//POST /todos/:id
+
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+	todos.push(body);
+	
+	res.json(body);
+});
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
