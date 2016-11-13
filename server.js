e28350eaa5dcbcedf45106ff771901f36a356e93
FenixDrive <davidbsmart@hotmail.com>
2016-11-13 19:38:54 +1300

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _  = require('underscore'); //Most people use the _ character for the variable requiring the underscore module
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -22,15 +23,24 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10); //You'll always want 10 unless you're using binary or hexidecimal
+	var matchedTodo = _.findWhere(todos, {id: todoId}); 
 
-	// Iterate over todo arrays. Find the match.
-	var matchedTodo;
 
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) { //Must use three = signs /Originall used todos instead of todo
-				matchedTodo = todo;
-		}
-	}); 
+
+	//The underscore library lets you use multiple functions. To see more checkout underscorejs.org
+
+
+		//This was the old code
+			// Iterate over todo arrays. Find the match.
+			// var matchedTodo;
+
+			// todos.forEach(function (todo) {
+			// 	if (todoId === todo.id) { //Must use three = signs /Originall used todos instead of todo
+			// 			matchedTodo = todo;
+			// 	}
+			// }); 
+
+
 
 		if (matchedTodo) {
 			res.json(matchedTodo);
@@ -45,11 +55,18 @@ app.get('/todos/:id', function (req, res) {
 //POST /todos/:id
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');  //Use _.pick to only pick description and completed
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){ 
+		return res.status(400).send();
+	}
+
+	// set body.description to be trimmed value
+	body.description = body.description.trim();
 
 	body.id = todoNextId++;
 	todos.push(body);
-	
+
 	res.json(body);
 });
 
